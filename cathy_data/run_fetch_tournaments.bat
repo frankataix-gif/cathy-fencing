c2979f6130e3aad02313c838b26c8e5281a74853
Cathy Parent <cathy@fencing.local>
2026-09-08 00:00:57 +0200

    fix: 修正本地 fetch .bat 的工作目录

--- cathy_data/run_fetch_tournaments.bat
+++ cathy_data/run_fetch_tournaments.bat
@@ -1,7 +1,7 @@
 @echo off
 chcp 65001 >nul
 
-cd /d "G:\我的云端硬盘\1 New 7-8\AgentAI\1"
+cd /d "G:\我的云端硬盘\1 Devin\02_个人家庭\01_Cathy Fencing"
 
 python "cathy_data\fetch_usa_fencing_tournaments.py"
 python "cathy_data\import_to_html.py"
