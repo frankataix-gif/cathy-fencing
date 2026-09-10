632ef36e6c90cedd4f00eff9f429e3a605abe310
frankataix-gif <frankataix@gmail.com>
2026-09-10 23:56:38 +0200

    Split home email card into Today and Latest sections

--- fencing_sw.js
+++ fencing_sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Cathy Fencing PWA
-const CACHE_NAME = 'cathy-fencing-v15';
+const CACHE_NAME = 'cathy-fencing-v16';
 const ASSETS = [
   './cathy_avatar.jpg',
   './manifest.json'
