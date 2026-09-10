9e90d96356384dafa79018b12de9b75bd65ac13b
frankataix-gif <frankataix@gmail.com>
2026-09-10 23:29:44 +0200

    Reorder schedule sub-tabs: tasks first, calendar second

--- fencing_sw.js
+++ fencing_sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Cathy Fencing PWA
-const CACHE_NAME = 'cathy-fencing-v10';
+const CACHE_NAME = 'cathy-fencing-v11';
 const ASSETS = [
   './cathy_avatar.jpg',
   './manifest.json'
