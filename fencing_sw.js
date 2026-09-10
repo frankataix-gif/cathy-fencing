40ddd714a39b488fec1927a7618cf2c411878884
frankataix-gif <frankataix@gmail.com>
2026-09-10 23:38:22 +0200

    Remember last active tab and sub-tab across reloads

--- fencing_sw.js
+++ fencing_sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Cathy Fencing PWA
-const CACHE_NAME = 'cathy-fencing-v12';
+const CACHE_NAME = 'cathy-fencing-v13';
 const ASSETS = [
   './cathy_avatar.jpg',
   './manifest.json'
