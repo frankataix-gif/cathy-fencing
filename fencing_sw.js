6eaa068bab1e3cc06d593f7159269df9758bbae8
frankataix-gif <frankataix@gmail.com>
2026-09-11 00:01:53 +0200

    Sort all emails by date descending newest first

--- fencing_sw.js
+++ fencing_sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Cathy Fencing PWA
-const CACHE_NAME = 'cathy-fencing-v16';
+const CACHE_NAME = 'cathy-fencing-v17';
 const ASSETS = [
   './cathy_avatar.jpg',
   './manifest.json'
