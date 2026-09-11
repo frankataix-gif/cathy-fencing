438ec5d101414e3dc3997e5fc64a4ae7c5841f41
frankataix-gif <frankataix@gmail.com>
2026-09-12 01:25:40 +0200

    Bump AI summary state version to force full re-analysis

--- fencing_sw.js
+++ fencing_sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Cathy Fencing PWA
-const CACHE_NAME = 'cathy-fencing-v28';
+const CACHE_NAME = 'cathy-fencing-v29';
 const ASSETS = [
   './cathy_avatar.jpg',
   './manifest.json'
