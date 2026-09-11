0c2798def17825d162accd5f2cc36922ee4577c7
frankataix-gif <frankataix@gmail.com>
2026-09-12 00:13:32 +0200

    Fix AI email summary timezone and bump cache to v24

--- fencing_sw.js
+++ fencing_sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Cathy Fencing PWA
-const CACHE_NAME = 'cathy-fencing-v23';
+const CACHE_NAME = 'cathy-fencing-v24';
 const ASSETS = [
   './cathy_avatar.jpg',
   './manifest.json'
