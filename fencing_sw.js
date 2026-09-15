e2f3cafd15b9a015a4526a95206672df3a91c36b
frankataix-gif <frankataix@gmail.com>
2026-09-16 00:15:41 +0200

    My tab: encrypted vault (AES-GCM+PBKDF2) for accounts/portals, synced to cathy_data/vault.json

--- fencing_sw.js
+++ fencing_sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Cathy Fencing PWA
-const CACHE_NAME = 'cathy-fencing-v33';
+const CACHE_NAME = 'cathy-fencing-v34';
 const ASSETS = [
   './cathy_avatar.jpg',
   './manifest.json',
