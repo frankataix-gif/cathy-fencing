8f53fcbe9341939fd7596e93b60cc48bf8ae6fbf
frankataix-gif <frankataix@gmail.com>
2026-09-15 22:34:02 +0200

    Update UX: top banner '有新功能更新' + tap-to-refresh without confirm; remove silent auto-reload and force-refresh confirm

--- fencing_sw.js
+++ fencing_sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Cathy Fencing PWA
-const CACHE_NAME = 'cathy-fencing-v32';
+const CACHE_NAME = 'cathy-fencing-v33';
 const ASSETS = [
   './cathy_avatar.jpg',
   './manifest.json',
